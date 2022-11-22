4c2edd9816327934be715407a008896b7220d399
pablocavaz04 <camvazpablo04@gmail.com>
2022-11-22 12:50:22 +0100

    First Commit

---
+++ prueba.java
@@ -0,0 +1,20 @@
+import  java.util.Scanner;
+
+public class prueba {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int [][] n = new int [5][5];
+        for(int i = 0; i < 5; i++)
+            for(int j = 0; j<5;j++)
+                if(i==j)
+                    n[i][j]=1;
+                else
+                    n[i][j]=0;
+
+        for(int i = 0; i < 5; i++){
+            for(int j = 0; j<5;j++)
+                System.out.print(n[i][j]+", ");
+            System.out.println();
+        }
+    } 
+}
