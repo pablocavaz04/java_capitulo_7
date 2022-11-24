85aab04c22636d3683cec60cea2fa46e24c87556
pablocavaz04 <camvazpablo04@gmail.com>
2022-11-24 22:31:15 +0100

    First Commit

--- Cap7Ejercicio08.java
+++ Cap7Ejercicio08.java
@@ -1,3 +1,10 @@
+/*
+ * Author: Pablo Camino Vázquez
+ * Realiza un programa que pida la temperatura media que ha hecho en cada mes
+de un determinado año y que muestre a continuación un diagrama de barras
+horizontales con esos datos. Las barras del diagrama se pueden dibujar a base
+de asteriscos o cualquier otro carácter.
+ */
 public class Cap7Ejercicio08 {
 
     public static void main(String[] args) {
---
+++ Cap7Ejercicio09.java
@@ -0,0 +1,27 @@
+/*
+ * Author: Pablo Camino Vázquez
+ * Realiza un programa que pida 8 números enteros y que luego muestre esos
+números junto con la palabra “par” o “impar” según proceda.
+ */
+import java.util.Scanner;
+public class Cap7Ejercicio09 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int [] numeros = new int[8];
+        for(int i=0;i<8;i++){
+            System.out.println("Introduzca numeros enteros");
+            numeros[i]=sc.nextInt();
+            }
+
+            for(int i=0;i<8;i++){
+                if(numeros[i]%2==0){
+                    System.out.println(numeros[i]+" par");
+                }else{
+                    System.out.println(numeros[i]+" impar");
+                }
+            }
+        
+
+        sc.close();
+    }
+}
